7eee11e4aa0355fa62d353c30d49771c42efdf1f
Kazibwe Julius Junior <kazibwejuliusjunior@gmail.com>
2019-04-14 10:16:45 +0300

    ..

--- resources/views/layouts/backend/main.blade.php
+++ resources/views/layouts/backend/main.blade.php
@@ -42,9 +42,9 @@
 
   <footer class="main-footer">
     <div class="pull-right hidden-xs">
-      <b>Version</b> 2.3.6
+      <b>Version</b> 1.0
     </div>
-    <strong>Copyright &copy; 2014-2016 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights
+    <strong>Copyright &copy; 2019 <a href="/">NDEBI TECH</a>.</strong> All rights
     reserved.
   </footer>
 
