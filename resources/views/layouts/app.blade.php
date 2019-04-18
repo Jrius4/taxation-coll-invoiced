5e276ba644e2e1f27cdeeb86312dca77aa328f6b
Kazibwe Julius Junior <kazibwejuliusjunior@gmail.com>
2019-04-18 09:13:45 +0300

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,8 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Log in</title>
+  <title>NDEBITECH | Log in</title>
+  <link rel="shortcut icon" href="/img/ndebi-tech-favi-blue.png" type="image/x-icon">
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.6 -->
